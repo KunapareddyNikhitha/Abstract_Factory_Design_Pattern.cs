cffde4f5f0a99e96f89fcfca37cbc46767499c15
Nikhitha <nikitha.kunapareddy@sailssoftware.com>
2023-05-01 12:00:22 +0530

    Add project files.

---
+++ ICar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory_Design_Pattern.cs
+{// The 'AbstractProductB' interface
+    // Each distinct product of the Car product family should have a base interface.
+    // All variants of the Car products must implement this ICar interface.
+    public interface ICar
+    {
+        void GetDetails();
+    }
+}
